c43254b75fb75faf8131be1db40f8547bac3e95b
aldobarrios81 <abv.mx81@gmail.com>
2022-05-09 18:31:02 -0500

    Limpieza de Codigo 090520221830

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,11 @@
-import logo from './../logo.svg';
-import './../App.css';
-
-function App() {
+function NavBar() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and saseload.
-        </p>
-        <a
-          className="App-link"
-          href="https://aldobarrios81.dev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
 }
 
-export default App;
+export default NavBar;
