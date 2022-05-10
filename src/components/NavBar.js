520fcd7b1d1fc3dba51d441a587c742872d45e3e
aldobarrios81 <abv.mx81@gmail.com>
2022-05-09 20:41:33 -0500

    Add Menu and Logo 090520222041

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,18 @@
 function NavBar() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
+    <div className="AppNavBar">
+      <header className="AppNavBar-header">
+        <div className="Logo">
+          <img src="./img/AldoBarriosDev.png" alt="Aldo"></img>
+        </div>
+        <div className="Menu">
+          <ul>
+            <li>Inicio</li>
+            <li>Aldo Barrios</li>
+            <li>Galeria</li>
+            <li>Contacto</li>
+          </ul>
+        </div>
       </header>
     </div>
   );
